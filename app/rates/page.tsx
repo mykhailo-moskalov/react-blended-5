b06bad183a0d7acea9bd51ed9d94ec7df521f601
mishokkartoshki <misha@moskalov.com>
2025-10-18 11:24:17 +0200

    rates

--- app/rates/page.tsx
+++ app/rates/page.tsx
@@ -7,9 +7,26 @@ import Section from '@/components/Section/Section';
 import Heading from '@/components/Heading/Heading';
 
 import css from './RatesPage.module.css';
+import RatesList from '@/components/RatesList/RatesList';
+import { useLatestRatesStore } from '@/lib/stores/currencyStore';
+import { useEffect } from 'react';
+import { latestRates } from '@/lib/service/exchangeAPI';
+import Filter from '@/components/Filter/Filter';
 
 export default function RatesPage() {
-  const isError = false;
+  const baseCurrency = useLatestRatesStore((state) => state.baseCurrency);
+  const isError = useLatestRatesStore((state) => state.isError);
+  const rates = useLatestRatesStore((state) => state.rates);
+  const setRates = useLatestRatesStore((state) => state.setRates);
+  const filter = useLatestRatesStore((state) => state.filter);
+
+  useEffect(() => {
+    latestRates(baseCurrency).then((rates) => setRates(rates));
+  }, []);
+
+  const filteredRates = rates
+    .filter(([key]) => key !== baseCurrency && key.toLowerCase().includes(filter))
+    .map(([key, value]) => ({ key, value: (1 / value).toFixed(2) }));
 
   return (
     <main className={css.main}>
@@ -20,12 +37,15 @@ export default function RatesPage() {
             bottom
             title={
               <Wave
-                text={`$ $ $ Current exchange rate for 1 ${'UAH'} $ $ $`}
+                text={`$ $ $ Current exchange rate for 1 ${baseCurrency} $ $ $`}
                 effect="fadeOut"
                 effectChange={4.0}
               />
             }
           />
+
+          <Filter />
+          {filteredRates.length > 0 && <RatesList rates={filteredRates} />}
 
           {isError && (
             <Heading error title="Something went wrong...😐 We cannot show current rates!" />
--- lib/stores/currencyStore.ts
+++ lib/stores/currencyStore.ts
@@ -1,1 +1,41 @@
-// lib\stores\currencyStore.ts
+import { create } from 'zustand';
+
+// interface RatesObj {
+//   key: string;
+//   value: number;
+// }
+
+type LatestRates = {
+  baseCurrency: string;
+  setBaseCurrency: (baseCurrency: string) => void;
+  exchangeInfo: null;
+  setExchangeInfo: (info: null) => void;
+  isLoading: boolean;
+  setIsLoading: () => void;
+  isError: boolean;
+  setIsError: () => void;
+  rates: [string, number][];
+  setRates: (rates: [string, number][]) => void;
+  filter: string;
+  setFilter: (filter: string) => void;
+};
+
+export const useLatestRatesStore = create<LatestRates>()((set) => ({
+  baseCurrency: '',
+  setBaseCurrency: (baseCurrency: string) => set(() => ({ baseCurrency })),
+
+  exchangeInfo: null,
+  setExchangeInfo: (info: null) => set(() => ({ exchangeInfo: info })),
+
+  isLoading: false,
+  setIsLoading: () => set(() => ({ isLoading: true })),
+
+  isError: false,
+  setIsError: () => set(() => ({ isError: true })),
+
+  rates: [],
+  setRates: (rates: [string, number][]) => set(() => ({ rates: rates })),
+
+  filter: '',
+  setFilter: (filter: string) => set(() => ({ filter: filter })),
+}));
